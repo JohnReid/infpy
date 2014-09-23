03f012abea29ec0e734d6eceafe44d08218ac2c3
JohnReid <johnbaronreid@netscape.net>
2014-09-23 08:40:34 +0100

    Update CCM. Now initialises.

--- python/infpy/crp/__init__.py
+++ python/infpy/crp/__init__.py
@@ -26,6 +26,15 @@ class CRP(object):
         "The number of tables."
         return len(self.m)
 
+    @property
+    def N(self):
+        "The number of data."
+        return len(self.z)
+
+    def __repr__(self):
+        "String representation."
+        return "CRP with {} tables for {} data".format(self.T, self.N)
+
     def issitting(self, n):
         "Is the n'th datum sitting down."
         return -1 != self.z[n]
@@ -49,7 +58,6 @@ class CRP(object):
         self.z[n] = t
         # Update the number of tables if it has increased.
         if t >= self.T:
-            self._T = t
             self.m.append(1)
         else:
             self.m[t] += 1
@@ -93,7 +101,69 @@ class CRP(object):
             self.sampleandsit(n, tablelikelihoodfn)
 
 
-class CRPMixture(object):
+class DPExpFam(object):
+
+    """A `Dirichlet process <http://en.wikipedia.org/wiki/Dirichlet_process>`_
+    that samples atoms from an exponential family conjugate prior.
+    """
+
+    def __init__(self, alpha, F, x, tau):
+        self.crp = CRP(alpha)
+        self.F = F
+        self.x = x
+        self.tau = tau
+        self.psi = []  # Posteriors for cluster parameters.
+
+    def tablelikelihood(self, n, t):
+        """The likelihood of the n'th datum given the parameter
+        of the t'th table."""
+        return npy.exp(self.tableloglikelihood(n, t))
+
+    def tableloglikelihood(self, n, t):
+        """The log likelihood of the n'th datum given the parameter
+        of the t'th table."""
+        if -1 == t:
+            psi = self.tau
+        else:
+            psi = self.psi[t]
+        logp = self.F.log_conjugate_predictive(self.x[n], psi)
+        _logger.debug('n=%d; t=%d, p=%.2e; T=%s, tau=%s',
+                      n, t, npy.exp(logp), self.x[n], psi)
+        return logp
+
+    @property
+    def N(self):
+        "Number of data."
+        return len(self.x)
+
+    def standup(self, n, t):
+        "Stand the n'th observation up from the t'th table."
+        self.psi[t] = self.F.add_observations_to_prior(-self.x[n],
+                                                       self.psi[t],
+                                                       n=-1)
+
+    def sitdown(self, n, t):
+        "Sit the n'th observation down at the t'th table."
+        if t >= len(self.psi):
+            assert t == len(self.psi)  # Can only have added one table
+            self.psi.append(self.tau.copy())
+            assert len(self.psi) == self.crp.T
+        self.psi[t] = self.F.add_observations_to_prior(self.x[n],
+                                                       self.psi[t],
+                                                       n=1)
+
+    def tablelikelihoodfn(self, n, t):
+        "The likelihood of the n'th datum under the t'th table's parameter."
+        # Is it a likelihood for a new table?
+        if -1 == t:
+            psi = self.tau
+        else:
+            psi = self.psi[t]
+        _logger.debug('n=%d; T=%s, tau=%s', n, self.x[n], psi)
+        return npy.exp(self.F.log_conjugate_predictive(self.x[n], psi))
+
+
+class DPMixture(DPExpFam):
 
     """A mixture of distributions from an exponential family
     implemented on top of a Chinese restaurant process.
@@ -112,7 +182,7 @@ class CRPMixture(object):
         >>> priormu = 0.5
         >>> priorlambda = 100.
         >>> tau = F.prior.eta((prioralpha, priorbeta, priormu, priorlambda))
-        >>> mixture = crp.CRPMixture(crpalpha, F, T, tau)
+        >>> mixture = crp.DPMixture(crpalpha, F, T, tau)
         >>> K = []
         >>> numsamples = 100
         >>> for i in xrange(numsamples):
@@ -122,28 +192,9 @@ class CRPMixture(object):
     """
 
     def __init__(self, alpha, F, x, tau):
-        self.crp = CRP(alpha)
-        self.F = F
-        self.x = x
-        self.tau = tau
-        self.psi = []  # Posteriors for cluster parameters.
+        super(DPMixture, self).__init__(alpha, F, x, tau)
         for n in xrange(self.N):
             self.sampleandsit(n)
-
-    @property
-    def N(self):
-        "Number of data."
-        return len(self.x)
-
-    def tablelikelihoodfn(self, n, t):
-        "The likelihood of the n'th datum under the t'th table's parameter."
-        # Is it a likelihood for a new table?
-        if -1 == t:
-            tau = self.tau
-        else:
-            tau = self.psi[t]
-        _logger.debug('n=%d; T=%s, tau=%s', n, self.x[n], tau)
-        return npy.exp(self.F.log_conjugate_predictive(self.x[n], tau))
 
     def sample(self):
         "Make one datum stand up and sit down."
@@ -152,160 +203,150 @@ class CRPMixture(object):
         self.standup(n, t)
         self.sampleandsit(n)
 
-    def standup(self, n, t):
-        self.psi[t] = self.F.add_observations_to_prior(-self.x[n], self.psi[t], n=-1)
-
-    def sitdown(self, n, t):
-        if t >= len(self.psi):
-            assert t == len(self.psi)  # Can only have added one table
-            self.psi.append(self.tau.copy())
-        self.psi[t] = self.F.add_observations_to_prior(self.x[n], self.psi[t], n=1)
-
     def sampleandsit(self, n):
-        t = self.crp.sampleandsit(n, self.tablelikelihoodfn)
+        t = self.crp.sampleandsit(n, self.tablelikelihood)
         self.sitdown(n, t)
         return t
 
 
-class DP(object):
+class ContextClusterMixture(object):
 
-    """A `Dirichlet process <http://en.wikipedia.org/wiki/Dirichlet_process>`_.
+    r"""
+    A context clustering mixture model. Each datum takes values in a set
+    of contexts.
+
+    - family = (H, F): A list of exponential family conjugate
+      prior/likelihood pairs (one for each context).
+    - x: The data, indexed by context first then datum.
+    - tau: A list of priors for each context.
+    - alpha: The parameter for the context level DPs.
+    - beta: The parameter for the overall DP.
+
+    If we have :math:`N` data each with values in :math:`J` contexts,
+    then we have the following model:
+
+    .. math::
+
+        G_j | H_j, \alpha \sim \textrm{DP}(H_j, \alpha)
+
+        G_0 | G_1, \dots, G_J, \beta \sim \textrm{DP}(G_1
+            \otimes \dots \otimes G_J, \beta)
+
+        \theta_n | G_0 \sim G_0
+
+        x_{n,j} | \theta_{n,j} \sim F_j(\theta_{n,j})
+
     """
 
-    def __init__(self, H, alpha):
-        self.crp = CRP(alpha)
-        self.H = H
-
-    def sample(self, n):
-        t = self.crp.sampletable(n, self.likelihoodfn)
-        if t >= len(self.atoms):
-            assert t == len(self.atoms)  # Can only have added one table
-        # Sample a new atom if it is a new table
-        self.atoms.append(self.H.sample())
-        return t
-
-    def likelihoodfn(self, t):
-        return 1.
-
-
-class CCModel(object):
-
-    def __init__(self, x, family, H, beta):
-        self.maxdim = max(f.dimension for f in family)
-        self.family = family
-        self.H = H
-        self.x = x
-        self.beta = beta
-        # Use -1 as indicator that table is not assigned
-        self.k = list()
-        self.z = - npy.ones(self.N)
-        self._initialisepsiprior()
+    def __init__(self, family, x, tau, alpha, beta):
+        lengths = map(len, x)
+        if not lengths:
+            raise ValueError('No contexts')
+        if max(lengths) != min(lengths):
+            raise ValueError('Different number of data in different contexts.')
+        self.N = lengths[0]
+        "Number of data."
+        self.contexts = [
+            self.Context(alpha, _family, _x, _tau)
+            for _family, _x, _tau
+            in zip(family, x, tau)
+        ]
+        "The contexts."
+        self.G0 = CRP(beta)
         self._initialisestate()
 
-    def _initialisepsiprior(self):
-        self.psiprior = npy.zeros((self.J, self.maxdim))
-        for j in xrange(self.J):
-            self.psiprior[j, :self.family[j].dimension] = self.H[j]
-        self.psi = [self.psiprior.copy()]
-
     def _initialisestate(self):
-        "Initialise the state by sampling z."
+        "Initialise the state sitting every datum down."
         for n in xrange(self.N):
-            self._samplez(n)
+            self.sampleandsit(n)
 
-    def _getJ(self):
-        return len(self.H)
+    @property
+    def J(self):
+        "The number of contexts."
+        return len(self.contexts)
 
-    J = property(_getJ, "The number of contexts.")
+    class Context(DPExpFam):
 
-    def _getN(self):
-        return len(self.x)
+        """Holds the context specific data and parameters."""
 
-    N = property(_getN, "The number of data.")
+        def __init__(self, alpha, F, x, tau):
+            super(ContextClusterMixture.Context, self).__init__(alpha, F, x,
+                                                                tau)
 
-    def _getT(self):
-        return len(self.k)
+        def likelihoods(self, n):
+            "The likelihood of the n'th datum for each table."
+            lls = npy.empty(self.crp.T + 1)
+            for t in xrange(self.crp.T):
+                if self.crp.m > 0:
+                    lls[t] = self.tableloglikelihood(n, t)
+                else:
+                    assert 0 == self.crp.m
+                    lls[t] = -npy.inf
+            lls[-1] = self.tableloglikelihood(n, -1)
+            return npy.exp(lls)
 
-    T = property(_getT, "The number of tables in G.")
+        def newtablelikelihood(self, likelihoods):
+            return (
+                self.crp.alpha * likelihoods[-1]  # new table
+                + npy.dot(self.crp.m, likelihoods[:-1])  # sum over existing
+            ) / (sum(self.crp.m) + self.crp.alpha)
 
-    def _getm(self):
-        m = npy.zeros(self.T, dtype=int)
-        for zn in self.z:
-            m[zn] += 1
-        return m
+    class Likelihoods(object):
 
-    m = property(_getm, "The number of customers at each table in G.")
+        """Calculates the likelihoods of each cluster context assignment."""
 
-    def _standup(self, n):
-        "Stand the n'th datum up from the table it is sitting at."
-        t = self.z[n]
-        if -1 != t:
-            self.z[n] = -1
-            self._sitattable(-self.x[n], t)
+        def __init__(self, contexts, n):
+            self.contexts = contexts
+            self.n = n
+            print 'Calculating likelihoods'
+            self.likelihoods = map(lambda context: context.likelihoods(n),
+                                   contexts)
 
-    def _sitdown(self, n, t):
-        "Sit the n'th datum down at the t'th table."
-        self.z[n] = t
-        self._sitattable(self.x[n], t)
-
-    def _sitattable(self, xn, t):
-        "Sit the datum at table t. This updates self.psi."
-        if t < 0 or t >= len(self.T):
-            raise ValueError("t=%d does not index a table" % t)
-        k = self.k[t]
-        for j, kj in enumerate(k):
-            self.psi[kj, j] += xn[j]
-
-    def _createnewtable(self):
-        "Sample a new table - initialise psi with the prior."
-        self.psi.append(self.psiprior.copy())
-
-    def _samplez(self, n):
-        "Sample a table assignment for n'th datum."
-        xn = self.x[n]
-        t = self.z[n]
-        # If they are already seated, we must make them stand up
-        self._standup(n)
-        # Calculate the probabilities to sit at any existing table
-        # or create a new one
-        p = npy.empty(self.T + 1)
-        m = self.m
-        emptytable = None  # Check for empty tables we can reuse
-        for t in range(self.T):
-            if 0 == m[t]:
-                emptytable = t
-                p[t] = 0
+        def tablelikelihood(self, n, t0):
+            """The likelihood for datum n sitting at table t0 in
+            the overall DP, G0."""
+            assert n == self.n
+            # Is it the likelihood for a new table in G0?
+            if -1 == t0:
+                # Yes - calculate the likelihood for each context as an
+                # integral over the context's tables
+                contextlikelihoods = (context.newtablelikelihood(likelihoods)
+                                      for context, likelihoods
+                                      in zip(self.contexts, self.likelihoods)
+                                      )
             else:
-                p[t] = m[t] * self.tablelikelihood(t, xn)
-        p[self.T] = self.beta * self.priorlikelihood(xn)
-        # Choose a table to sit at
-        t = npy.random.choice(self.T + 1, p)
-        # If we chose a new table, we must create it unless we can
-        # reuse an old empty one
-        if t == self.T:
-            if emptytable is None:
-                self._createnewtable()
-            else:
-                t = emptytable
-        # Sit down
-        del m  # m no longer valid once we sit down
-        self.sitdown(n, t)
+                # No - calculate the likelihood for each context by
+                # picking out the context's table that the table in G0
+                # is associated with
+                contextlikelihoods = (likelihoods[context.crp.z[t0]]
+                                      for context, likelihoods
+                                      in zip(self.contexts, self.likelihoods)
+                                      )
+            # Return the product over all contexts
+            return reduce(float.__mul__, contextlikelihoods, 1.)
 
-    def priorlikelihood(self, xn):
-        "The likelihood of xn given the priors for each context."
-        return npy.exp(sum(
-            family.log_p_T(xnj, eta)
-            for family, eta, xnj
-            in zip(self.family, self.psiprior, xn)
-        ))
+        def contexttablelikelihoodfn(self, j):
+            """Return a function that calculates the likelihood for the
+            n'th datum sitting at the tj'th table in context j.
+            """
+            def contexttablelikelihood(n, tj):
+                return self.likelihoods[j][tj]
+            return contexttablelikelihood
 
-    def tablelikelihood(self, t, xn):
-        "The likelihood of xn given the parameters of table t."
-        return npy.exp(sum(
-            family.log_p_T(xnj, self.psi[k][j])
-            for family, k, xnj, j
-            in zip(self.family, self.k[t], xn, xrange(self.J))
-        ))
+    def sampleandsit(self, n):
+        likelihoods = self.Likelihoods(self.contexts, n)
+        t0 = self.G0.sampleandsit(n, likelihoods.tablelikelihood)
+        # Did we sit at a new table in G0?
+        if 1 == self.G0.m[t0]:
+            # Yes so choose a table in each context level DP
+            for j, context in enumerate(self.contexts):
+                context.crp.sampleandsit(
+                    t0,
+                    likelihoods.contexttablelikelihoodfn(j))
+        # Update psi at context level
+        for context in self.contexts:
+            tj = context.crp.z[t0]
+            context.sitdown(n, tj)
 
 
 if '__main__' == __name__:
@@ -338,6 +379,7 @@ if '__main__' == __name__:
     n, bins, patches = pyl.hist(sds, 50, histtype='stepfilled')
     pyl.savefig('sds.pdf')
 
+    # Test the CRP mixture model
     import infpy.exp
     reload(infpy.exp)
     import infpy.crp
@@ -360,20 +402,34 @@ if '__main__' == __name__:
         mixture.sample()
         K.append(len(npy.unique(mixture.crp.z)))
 
+    # Test the context clustering model
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    import infpy.exp
+    reload(infpy.exp)
+    import infpy.exp as exp
+    import numpy as npy
+    F = exp.GaussianConjugatePrior()
+    # Use two contexts with same exponential family
+    x = [
+        npy.array([0,  0,  0,  1,  1,  1]),
+        npy.array([0,  0,  1,  1, -1, -1]),
+        ]
+    T = map(F.likelihood.T, x)  # Convert to exponential family suff. stats
+    alpha = 1.
+    beta = 1.
+    prioralpha = 100.
+    priorbeta = 1.
+    priormu = 0.0
+    priorlambda = 100.
+    tau = F.prior.eta((prioralpha, priorbeta, priormu, priorlambda))
+    import infpy.crp
+    reload(infpy.crp)
+    ccm = infpy.crp.ContextClusterMixture([F] * len(x), T, [tau] * len(x),
+                                          alpha, beta)
+
+    for i in xrange(1000):
+        mixture.sample()
+        K.append(len(npy.unique(mixture.crp.z)))
     # import pandas as pd
     # tcga = pd.read_csv("../data/TCGA.csv.gz", compression="gzip")
-    import infpy.exp as exp
-    families = [
-        exp.GaussianExpFamily(dimension=1),
-        exp.GaussianExpFamily(dimension=1),
-    ]
-    x = npy.array([
-        [1.0, 1],
-        [1.0, 1],
-        [1.0, 1],
-        [0.0, 2],
-        [1.0, 2],
-        [0.0, 2],
-        [0.0, 2],
-    ])
-    model = CCModel(x)
